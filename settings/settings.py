3fc7f1ef3d13b48ac33cca8ca73820d10ecf2ba1
Ramazon Ergashev <b92874193@gmail.com>
2024-05-06 14:53:30 +0500

    sdvfgsdv

--- settings/settings.py
+++ settings/settings.py
@@ -60,13 +60,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'settings.wsgi.app'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE" : "django.db.backends.postgresql",
+        "NAME" : os.environ['DBNAME'],
+        "USER" : os.environ['DBUSER'],
+        "PASSWORD" : os.environ['DBPASSWORD'],
+        "HOST" : os.environ['DBHOST'],
+        "PORT" : os.environ['DBPORT']
     }
 }
 
