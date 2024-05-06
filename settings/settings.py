c30daecc59449e80078756caf8b9d28cffc3b553
Ramazon Ergashev <b92874193@gmail.com>
2024-05-06 15:14:32 +0500

    vsdd

--- settings/settings.py
+++ settings/settings.py
@@ -108,10 +108,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media_root')
 
 
 # Default primary key field type
